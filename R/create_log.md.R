7273e13cbe2323db4c59d45382bced998e5fddbd
Meera Patel <patelm9@mskcc.org>
2020-08-11 09:13:04 -0400

    modify create_log.md function

--- R/create_log.md.R
+++ R/create_log.md.R
@@ -7,14 +7,13 @@ create_log.md <-
         function() {
                 if (!file.exists("log.md")) {
 
-                        new_log <- rubix::blank_tibble(column_names =  c(
-                                                                        "Timestamp",
-                                                                        "Activity Type",
-                                                                        "Function",
-                                                                        "File",
-                                                                        "Details"
-                                                                        )
-                                                       )
+                        new_log <-
+                        tibble::tribble(~Timestamp,
+                                        ~`Activity Type`,
+                                        ~Function,
+                                        ~File,
+                                        ~Details)
+
                         readr::write_tsv(new_log,
                                          path = "log.md")
                 }
